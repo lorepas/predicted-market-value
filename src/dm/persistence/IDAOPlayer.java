1b19d9f422cfeccebf2a6dfc99bd87833a6c8fac
lorepas <pascolorenzo@gmail.com>
2020-02-19 16:54:12 +0100

    Fixed some bug on model

--- src/dm/persistence/IDAOPlayer.java
+++ src/dm/persistence/IDAOPlayer.java
@@ -6,6 +6,6 @@ import dm.model.PerformanceLastSeason;
 public interface IDAOPlayer {
 	
 	List<PerformanceLastSeason> retrieveOverallStatisticsLastSeason() throws DAOException;
-	double retrieveAvgMarketValueFromPlayer(String name) throws DAOException;
+	//double retrieveAvgMarketValueFromPlayer(String name) throws DAOException;
 	
 }
