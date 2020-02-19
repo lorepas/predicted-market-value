1729d1e410c17cb43c0a5b75a443d9ea3e57f032
lorepas <pascolorenzo@gmail.com>
2020-02-19 22:56:49 +0100

    Added the classifier

--- src/dm/classifier/RandomTreeClassifier.java
+++ src/dm/classifier/RandomTreeClassifier.java
@@ -2,9 +2,12 @@ package dm.classifier;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.Random;
 
 import javax.swing.JFileChooser;
 
+import weka.classifiers.evaluation.Evaluation;
+import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
 import weka.core.converters.CSVLoader;
 import weka.core.converters.ConverterUtils.DataSource;
@@ -13,25 +16,26 @@ public class RandomTreeClassifier {
 	
 	public void classifier() throws Exception {
 		String defaultDirectoryPath = new JFileChooser().getFileSystemView().getDefaultDirectory().toString();
-		//DataSource source = new DataSource("\\Users\\loren\\OneDrive\\Documenti\\playerStatistic.csv");
+		//DataSource source = new DataSource(defaultDirectoryPath+"\\playerStatistics.arff");
 		CSVLoader csv = new CSVLoader();
 		csv.setSource(new File(defaultDirectoryPath+"\\playerStatistic.csv"));
 		csv.setNoHeaderRowPresent(false);
 		csv.setFieldSeparator(";");
-//		csv.setDateAttributes("5");
-//		csv.setDateFormat("dd/MM/yyyy");
-//		csv.setNominalAttributes("1-4");
+		csv.setDateAttributes("5");
+		csv.setDateFormat("dd/MM/yyyy");
+		csv.setNominalAttributes("1-4");
 		Instances trainDataset = csv.getDataSet();
-		System.out.println(trainDataset.toString());
+		System.out.println(trainDataset);
 		//Setting the classes
 		trainDataset.setClassIndex(trainDataset.numAttributes()-1);
-		//Removing the season
-		trainDataset.remove(0);
-		int numClasses = trainDataset.numClasses();
-		System.out.println(numClasses);
-		for(int i=0; i<numClasses; i++) {
-			String classValue = trainDataset.classAttribute().value(i);
-			System.out.println("Class "+i+" is "+classValue);
-		}
+		String[] options = new String[2];
+		options[0]="-print";
+		options[1]="-attribute-importance";
+		RandomForest randomForest = new RandomForest(); 
+		randomForest.setOptions(options);
+		randomForest.buildClassifier(trainDataset);
+		Evaluation eval = new Evaluation(trainDataset);
+		eval.crossValidateModel(randomForest, trainDataset, 10, new Random(1));
+		System.out.println(eval.toSummaryString("\n Results \n\n",false));
 	}
 }
