c2e93d79eb8de9889a2e032362665175317a3d0b
lorepas <pascolorenzo@gmail.com>
2020-02-22 15:30:24 +0100

    Added the prevision of market value

--- src/dm/classifier/RandomTreeClassifier.java
+++ src/dm/classifier/RandomTreeClassifier.java
@@ -20,6 +20,7 @@ import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.SparseInstance;
 import weka.core.Utils;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
@@ -41,6 +42,7 @@ public class RandomTreeClassifier {
 		csv.setDateFormat("dd/MM/yyyy");
 		csv.setFieldSeparator(";");
 		Instances trainDataset = csv.getDataSet();
+		trainDataset.deleteAttributeAt(0);
 		System.out.println(trainDataset);
 		List teamNominal = new ArrayList();
 		List nationNominal = new ArrayList();
@@ -65,41 +67,42 @@ public class RandomTreeClassifier {
 //		String[] options = new String[2];
 //		options[0]="-print";
 //		options[1]="-attribute-importance";
-//		RandomForest randomForest = new RandomForest(); 
+//		 
 //		randomForest.setOptions(options);
-//		randomForest.buildClassifier(trainDataset);
-//		Evaluation eval = new Evaluation(trainDataset);
-//		eval.crossValidateModel(randomForest, trainDataset, 10, new Random(1));
-//		System.out.println(eval.toSummaryString("\nResults:\n\n",false));
+		RandomForest randomForest = new RandomForest();
+		randomForest.buildClassifier(trainDataset);
+		Evaluation eval = new Evaluation(trainDataset);
+		eval.crossValidateModel(randomForest, trainDataset, 10, new Random(1));
+		System.out.println(eval.toSummaryString("\nResults:\n\n",false));
 
 		//Creating new dataset for new statistics
-		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
-		//Creating the dataset header
-		attributes.add(new Attribute("TEAM",teamNominal));
-		attributes.add(new Attribute("NATION",nationNominal));
-		attributes.add(new Attribute("ROLE",roleNominal));
-		attributes.add(new Attribute("BORN DATE","dd/MM/yyyy"));
-		attributes.add(new Attribute("CALLS"));
-		attributes.add(new Attribute("PRESENCES"));
-		attributes.add(new Attribute("GOALS"));
-		attributes.add(new Attribute("ASSISTS"));
-		attributes.add(new Attribute("PENALTY GOALS"));
-		attributes.add(new Attribute("OWN GOALS"));
-		attributes.add(new Attribute("YELLOW CARDS"));
-		attributes.add(new Attribute("DOUBLE YELLOW CARDS"));
-		attributes.add(new Attribute("RED CARDS"));
-		attributes.add(new Attribute("MINUTES PLAYED"));
-		attributes.add(new Attribute("MARKET VALUE"));
-	
-		Instances newDataset = new Instances("NewPlayer",attributes,0);
-		newDataset.setClassIndex(newDataset.numAttributes()-1);
-
-		// adding instances
-		double[] values = new double[newDataset.numAttributes()];
-		values[0] = newDataset.attribute(0).indexOfValue(team);
-		values[1] = newDataset.attribute(1).indexOfValue(nation);
-		values[2] = newDataset.attribute(2).indexOfValue(role);
-		values[3] = newDataset.attribute(3).parseDate(bornDate);
+//		ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+//		//Creating the dataset header
+//		attributes.add(new Attribute("TEAM",teamNominal));
+//		attributes.add(new Attribute("NATION",nationNominal));
+//		attributes.add(new Attribute("ROLE",roleNominal));
+//		attributes.add(new Attribute("BORN DATE","dd/MM/yyyy"));
+//		attributes.add(new Attribute("CALLS"));
+//		attributes.add(new Attribute("PRESENCES"));
+//		attributes.add(new Attribute("GOALS"));
+//		attributes.add(new Attribute("ASSISTS"));
+//		attributes.add(new Attribute("PENALTY GOALS"));
+//		attributes.add(new Attribute("OWN GOALS"));
+//		attributes.add(new Attribute("YELLOW CARDS"));
+//		attributes.add(new Attribute("DOUBLE YELLOW CARDS"));
+//		attributes.add(new Attribute("RED CARDS"));
+//		attributes.add(new Attribute("MINUTES PLAYED"));
+//		attributes.add(new Attribute("MARKET VALUE"));
+//	
+//		Instances newDataset = new Instances("NewPlayer",attributes,1);
+//		newDataset.setClassIndex(newDataset.numAttributes()-1);
+//
+//		// adding instances
+		double[] values = new double[trainDataset.numAttributes()];
+		values[0] = trainDataset.attribute(0).indexOfValue(team);
+		values[1] = trainDataset.attribute(1).indexOfValue(nation);
+		values[2] = trainDataset.attribute(2).indexOfValue(role);
+		values[3] = trainDataset.attribute(3).parseDate(bornDate);
 		values[4] = calls;
 		values[5] = presences;
 		values[6] = goals;
@@ -112,10 +115,16 @@ public class RandomTreeClassifier {
 		values[13] = minutesPlayed;
 		values[14] = Utils.missingValue();
 		Instance inst = new DenseInstance(1.0,values);
-		newDataset.add(inst);
-		newDataset.get(0).setClassMissing();
-		System.out.println(newDataset);
+//		inst.setDataset(newDataset);
+//		newDataset.add(inst);
+//		newDataset.get(0).setClassMissing();
+//		System.out.println(newDataset);
+//		System.out.println("\nEstimate Market Value:\n");
+//		System.out.println(randomForest.classifyInstance(newDataset.firstInstance()));
+		trainDataset.add(inst);
+		System.out.println(trainDataset);
+		
 		System.out.println("\nEstimate Market Value:\n");
-		//System.out.println(randomForest.classifyInstance(inst));
+		System.out.println(randomForest.classifyInstance(trainDataset.lastInstance()));
 	}
 }
